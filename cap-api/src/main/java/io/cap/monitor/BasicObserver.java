387f4f506f070d1e02581e5df05db211738134a3
Chris Kelly <christopherjkelly@gmail.com>
2018-06-10 09:30:04 +0200

    Licensing config.

--- cap-api/src/main/java/io/cap/monitor/BasicObserver.java
+++ cap-api/src/main/java/io/cap/monitor/BasicObserver.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018 - 2018, Chris Kelly, all rights reserved.
+ *
+ * This software is licensed under under GPL-3.0-only or GPL-3.0-or-later, https://opensource.org/licenses/GPL-3.0
+ */
+
 package io.cap.monitor;
 
 import java.util.ArrayList;
