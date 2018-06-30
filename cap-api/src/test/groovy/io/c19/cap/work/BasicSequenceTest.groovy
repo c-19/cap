8d5acdbc8a281015ef804473508ef4dda63759c3
Chris Kelly <christopherjkelly@gmail.com>
2018-06-30 16:49:08 +0200

    Updated copyright to C19.

--- cap-api/src/test/groovy/io/c19/cap/work/BasicSequenceTest.groovy
+++ cap-api/src/test/groovy/io/c19/cap/work/BasicSequenceTest.groovy
@@ -1,7 +1,8 @@
 /*
- * Copyright (c) 2018 - 2018, Chris Kelly, all rights reserved.
+ * Copyright (c) 2018 - 2018, C19, all rights reserved.
  *
  * This software is licensed under under GPL-3.0-only or GPL-3.0-or-later, https://opensource.org/licenses/GPL-3.0
+ *
  */
 
 package io.c19.cap.work
